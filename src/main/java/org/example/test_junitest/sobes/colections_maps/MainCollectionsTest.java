fe59c0560d93b443bae46412215e558863224272
s11110101111 <s11110101111@gmail.com>
2021-01-09 09:06:26 +0500

    text list

--- src/main/java/org/example/test_junitest/sobes/colections_maps/MainCollectionsTest.java
+++ src/main/java/org/example/test_junitest/sobes/colections_maps/MainCollectionsTest.java
@@ -1,10 +1,5 @@
 package org.example.test_junitest.sobes.colections_maps;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainCollectionsTest {
 
