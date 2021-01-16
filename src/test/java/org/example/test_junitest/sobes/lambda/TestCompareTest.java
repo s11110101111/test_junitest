1f42a1275300c88873e5e86dfe20a6f1e1ecff43
s11110101111 <s11110101111@gmail.com>
2021-01-16 23:50:12 +0500

    test comparator

--- src/test/java/org/example/test_junitest/sobes/lambda/TestCompareTest.java
+++ src/test/java/org/example/test_junitest/sobes/lambda/TestCompareTest.java
@@ -9,7 +9,7 @@ public class TestCompareTest {
 
     @Test
     public void TestComparatorLambda() {
-        Comparator<Integer> comp = ((x, y) -> (x > y) ? 1 : ((x == y) ? 0 : -1));
+        Comparator<Integer> comp = ((x, y) -> (x > y) ? 1 : ((x < y) ? -1 : 0));
         Assert.assertEquals(0,comp.compare(0,0));
         Assert.assertEquals(1,comp.compare(100,99));
         Assert.assertEquals(-1, comp.compare(99,100));
