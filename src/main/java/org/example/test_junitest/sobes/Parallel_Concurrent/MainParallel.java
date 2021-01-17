7aa4cdf125a8bc289fa093be13404ff9f63fe701
s11110101111 <s11110101111@gmail.com>
2021-01-17 19:12:01 +0500

    Threads synchronize test -1 -2

--- src/main/java/org/example/test_junitest/sobes/Parallel_Concurrent/MainParallel.java
+++ src/main/java/org/example/test_junitest/sobes/Parallel_Concurrent/MainParallel.java
@@ -6,20 +6,22 @@ public class MainParallel {
     private static int count = 0;
 
     public static void main(String[] args) {
-        test1();
+        test2();
     }
 
     public static void test1() {
         Runnable rn = new Runnable() {
             @Override
             public void run() {
-                count = 0;
-                for (int i = 0; i < 5; i++) {
-                    count = count + 1;
-                    if (Thread.currentThread().getName().equals("Thread-1")) {
-                        System.out.println(ColorANSI.PURPLE.fillColor(Thread.currentThread().getName() + " " + count));
-                    } else {
-                        System.out.println(ColorANSI.YELLOW.fillColor(Thread.currentThread().getName() + " " + count));
+                synchronized (this) {
+                    count = 0;
+                    for (int i = 0; i < 5; i++) {
+                        count = count + 1;
+                        if (Thread.currentThread().getName().equals("Thread-1")) {
+                            System.out.println(ColorANSI.PURPLE.fillColor(Thread.currentThread().getName() + " " + count));
+                        } else {
+                            System.out.println(ColorANSI.YELLOW.fillColor(Thread.currentThread().getName() + " " + count));
+                        }
                     }
                 }
             }
@@ -33,6 +35,55 @@ public class MainParallel {
     }
 
     public static void test2() {
+        StringBuilder stb = new StringBuilder();
+        Runnable rnb = new Runnable() {
+            @Override
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                String text = " it always seems impossible until it's done. Hello Hi Hello Pure Pure Pleasure HUUUU Foooo Hello Hi Hello Pure Pure Pleasure HUUUU Foooo ";
+                if (threadName.equals("Thread-0")) {
+                    threadName = ColorANSI.RED.fillColor(threadName);
+                    text= ColorANSI.RED.fillColor(text);
+                }
+                if (threadName.equals("Thread-1")) {
+                    threadName = ColorANSI.YELLOW.fillColor(threadName);
+                    text= ColorANSI.YELLOW.fillColor(text);
+                }
+                if (threadName.equals("Thread-2")) {
+                    threadName = ColorANSI.PURPLE.fillColor(threadName);
+                    text= ColorANSI.PURPLE.fillColor(text);
+                }
+                if (threadName.equals("Thread-3")) {
+                    threadName = ColorANSI.BLUE.fillColor(threadName);
+                    text= ColorANSI.BLUE.fillColor(text);
+                }
+
+                stb
+                        .append(threadName)
+                        .append("\n")
+                        .append(text)
+                        .append("\n");
+            }
+        };
+        Thread thr = new Thread(rnb);
+        Thread thr1 = new Thread(rnb);
+        Thread thr2 = new Thread(rnb);
+        Thread thr3 = new Thread(rnb);
+
+        thr.start();
+        thr1.start();
+        thr2.start();
+        thr3.start();
+
+        try {
+            thr.join();
+            thr1.join();
+            thr2.join();
+            thr3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(stb);
 
     }
 
