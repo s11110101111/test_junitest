15ad3ceb1e1087f5a533594ea575bc871744e2c1
s11110101111 <s11110101111@gmail.com>
2021-01-19 16:46:35 +0500

    add example concurrent  demo

---
+++ src/main/java/org/example/test_junitest/sobes/Parallel_Concurrent/multi_threads/multithreading2/demo6/ParallelStream.java
@@ -0,0 +1,23 @@
+package org.example.test_junitest.sobes.Parallel_Concurrent.multi_threads.multithreading2.demo6;
+
+import java.util.Arrays;
+
+public class ParallelStream {
+
+    public static void main(String[] args) {
+        int[] array = Commons.prepareArray();
+
+
+        long startTime = System.currentTimeMillis();
+
+        double sum = Arrays.stream(array)
+                .parallel()
+                .mapToDouble(Commons::function)
+                .sum();
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("sum = " + sum);
+        System.out.println("time = " + (endTime - startTime) + "ms");
+    }
+}
