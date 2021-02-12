ad98baa48316dc5ba8eac5a40b88c77d9dd8c043
s11110101111 <s11110101111@gmail.com>
2021-02-12 22:26:40 +0500

    синглетон синхр volatile

--- src/main/java/org/example/test_junitest/sobes/singletons/Singleton4Volatile.java
+++ src/main/java/org/example/test_junitest/sobes/singletons/Singleton4Volatile.java
@@ -2,7 +2,7 @@ package org.example.test_junitest.sobes.singletons;
 
 public class Singleton4Volatile {
     private static volatile Singleton4Volatile singleton;
-
+// это добавили тк оптимизация java удаляла 2 If )
     public Singleton4Volatile() {
     }
 
