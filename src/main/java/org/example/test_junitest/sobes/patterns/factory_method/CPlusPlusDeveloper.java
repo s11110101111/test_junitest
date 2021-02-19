7bc37163973c41201755c11452bcb21cabc6502a
s11110101111 <s11110101111@gmail.com>
2021-02-19 11:32:13 +0500

    Pattern Factory method

--- src/main/java/org/example/test_junitest/sobes/patterns/factory_method/CPlusPlusDeveloper.java
+++
@@ -1,8 +0,0 @@
-package org.example.test_junitest.sobes.patterns.factory_method;
-
-public class CPlusPlusDeveloper implements Developers {
-    @Override
-    public void writeCode() {
-System.out.println("Im super C++ coder)");
-    }
-}
