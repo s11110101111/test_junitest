7122c376e89bd66939ea6c6449fd9f0b2df1650e
s11110101111 <s11110101111@gmail.com>
2022-01-09 14:07:38 +0500

    add TestHelper and test for BigDecimalTask

--- src/test/java/org/example/test_junitest/hacker_rank_solutions/big_decimal/BigDecimalTaskTest.java
+++ src/test/java/org/example/test_junitest/hacker_rank_solutions/big_decimal/BigDecimalTaskTest.java
@@ -3,13 +3,14 @@ package org.example.test_junitest.hacker_rank_solutions.big_decimal;
 import org.example.test_junitest.test_helper.TestHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("HackerRank solution BigDecimal task.")
 class BigDecimalTaskTest {
     private BigDecimalTask bgt = TestHelper.getInstance(BigDecimalTask.class);
     String[] bigElements = {"-100","50","0","56.6","90","0.12",".12","02.34","000.000"};
@@ -17,6 +18,7 @@ class BigDecimalTaskTest {
     //  но мы обработаем уже готовый массив строк с цифрами
 
     @Test
+
     void runBigDecimal() {
         bgt.runBigDecimal(bigElements);
         String actualString = Arrays.toString(bigElements);
