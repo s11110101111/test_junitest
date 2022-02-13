f9f0060207e8a1a12cd41b32cfc8b83ee7393db7
sykutejnik <sykutejnik@mts.ru>
2022-02-14 00:01:12 +0500

    CommitRebuild4.0

---
+++ src/main/java/org/example/test_junitest/git/SomeClass.java
@@ -0,0 +1,12 @@
+package org.example.test_junitest.git;
+
+/**
+ *
+ */
+
+public class SomeClass {
+
+    private String firstCommit;
+    private String secondCommit;
+    private String _3Commit;
+}
