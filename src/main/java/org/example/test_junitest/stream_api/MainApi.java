6b2ba1fba08ebcaf62b8e905ea4f1c16cd036225
sykutejnik <sykutejnik@mts.ru>
2022-02-23 16:32:12 +0500

    fix stream
    Signed-off-by: sykutejnik <sykutejnik@mts.ru>

--- src/main/java/org/example/test_junitest/stream_api/MainApi.java
+++ src/main/java/org/example/test_junitest/stream_api/MainApi.java
@@ -13,18 +13,22 @@ public class MainApi {
     static {
 
         //stream from collections
-        Stream<String> sString = Arrays.asList("Java", "C++", "Pyton", "ruby", "java", "Hello World").stream();
+        Stream<String> sString = Arrays.asList("Java", "C++", "Pyton", "ruby", "java",
+            "Hello World").stream();
         mapStream.put("streamStringOfCollection", sString);
         // stream from value /значений
         Stream<Integer> sInt = Stream.of(1, 22, 3, 4, 67, 13, 2, 4, 56, 56, 6, 9, 10, 10);
         mapStream.put("streamIntegerOfValue", sInt);
         //stream from arrays
         String[] arrStr = {"A1", "a2", "a2", "A1", "aa12", "bb12", "a12", "A34"};
-        Stream<String> s = Arrays.stream(arrStr);
+
+        Stream<String> s1 = Stream.of(arrStr);
+        mapStream.put("streamArrayString", s1);
 //add of char cast intstream
-        IntStream ii =  "HelloWorld new year , very good".chars();//IntStream
+        IntStream ii = "HelloWorld new year , very good".chars();//IntStream
         mapStream.put("streamIntOfChars", ii.boxed());
-        Stream<Object> object = Stream.builder().add("bb1").add("bb2").add("bb3").add("bb4").add("bb4").build();
+        Stream<Object> object = Stream.builder().add("bb1").add("bb2").add("bb3").add("bb4")
+            .add("bb4").build();
 // iterator
         sInt = Stream.iterate(0, (i) -> i + 1).limit(100L);
         mapStream.put("streamIntIterator", sInt);
@@ -35,12 +39,17 @@ public class MainApi {
     }
 
     public static void main(String[] args) {
-        for(Map.Entry<String, Stream<?>> entry: mapStream.entrySet()){
+        for (Map.Entry<String, Stream<?>> entry : mapStream.entrySet()) {
             System.out.println(entry.getKey());
             entry.getValue().forEach(System.out::println);
 
 
         }
+//не полетит так
+// как стримы закончились
+        //   Stream<?> stream1 = getStream("streamArrayString");
+        //      stream1.map(s->s.toString().toUpperCase() ).forEach(System.out::println);
+
     }
 
     private static Stream getStream(String type) {
