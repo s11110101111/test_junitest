7db78df75fa82beff405c043b9740162f926bd64
sykutejnik <sykutejnik@mts.ru>
2022-03-27 13:16:36 +0500

    new task Array1DPart2Solution add tdd test red
    Signed-off-by: sykutejnik <sykutejnik@mts.ru>

---
+++ src/main/java/org/example/test_junitest/hacker_rank_solutions/data_structures/array_1d_part2/Array1DPart2Solution.java
@@ -0,0 +1,11 @@
+package org.example.test_junitest.hacker_rank_solutions.data_structures.array_1d_part2;
+
+/**
+ *
+ */
+
+public interface Array1DPart2Solution {
+
+    boolean canWin(int leap, int[] game);
+
+}
---
+++ src/test/java/org/example/test_junitest/hacker_rank_solutions/data_structures/array_1d_part2/Array1DPart2SolutionTest.java
@@ -0,0 +1,34 @@
+package org.example.test_junitest.hacker_rank_solutions.data_structures.array_1d_part2;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ *
+ */
+
+class Array1DPart2SolutionTest {
+
+    Array1DPart2Solution array1DPart2Solution;
+
+    @ParameterizedTest
+    @MethodSource("getParamForGameTest")
+    void isWinWithTwoArgs(int leap, int[] game, boolean isWin) {
+        Assertions.assertThat(array1DPart2Solution.canWin(leap, game)).isEqualTo(isWin);
+
+    }
+
+    static Stream<Arguments> getParamForGameTest() {
+        return Stream.of(
+            arguments(3, new int[]{0, 0, 0, 0, 0}, true),
+            arguments(5, new int[]{0, 0, 0, 1, 1, 1}, true),
+            arguments(3, new int[]{0, 0, 1, 1, 1, 1, 0}, false),
+            arguments(1, new int[]{0, 1, 0}, false));
+    }
+
+}
